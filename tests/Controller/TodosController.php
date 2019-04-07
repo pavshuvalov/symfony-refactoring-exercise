22792b51d46f393a2e5d1145e69ffeaf4f13420c
Pavel Shuvalov <echvera@icloud.com>
2019-04-07 23:35:39 +0500

    update tests

--- tests/Controller/TodosController.php
+++ tests/Controller/TodosController.php
@@ -9,14 +9,14 @@ class TodosControllerTest extends WebTestCase
 {
 	public function testGetTodos()
 	{
-		$c = static::createClient();
-		$c->request('GET', '/todo');
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+		$client->request('GET', '/todo');
 
 		// is ok
-		$this->assertEquals(Response::HTTP_OK, $c->getResponse()->getStatusCode());
+		$this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
 
 		// test response
-		$response = json_decode($c->getResponse()->getContent(), true);
+		$response = json_decode($client->getResponse()->getContent(), true);
 		$this->assertCount(2, $response);
 		$this->assertArrayHasKey('status', $response);
 		$this->assertEquals('ok', $response['status']);
@@ -31,14 +31,14 @@ class TodosControllerTest extends WebTestCase
 
 	public function testGetTodosLengthLt1()
 	{
-		$c = static::createClient();
-		$c->request('GET', '/todo?length=0');
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+		$client->request('GET', '/todo?length=0');
 
 		// is bad request
-		$this->assertEquals(Response::HTTP_BAD_REQUEST, $c->getResponse()->getStatusCode());
+		$this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
 
 		// test response
-		$response = json_decode($c->getResponse()->getContent(), true);
+		$response = json_decode($client->getResponse()->getContent(), true);
 		$this->assertCount(2, $response);
 		$this->assertArrayHasKey('status', $response);
 		$this->assertEquals('error', $response['status']);
@@ -48,14 +48,14 @@ class TodosControllerTest extends WebTestCase
 
 	public function testGetTodosLengthGt200()
 	{
-		$c = static::createClient();
-		$c->request('GET', '/todo?length=201');
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+		$client->request('GET', '/todo?length=201');
 
 		// is bad request
-		$this->assertEquals(Response::HTTP_BAD_REQUEST, $c->getResponse()->getStatusCode());
+		$this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
 
 		// test response
-		$response = json_decode($c->getResponse()->getContent(), true);
+		$response = json_decode($client->getResponse()->getContent(), true);
 		$this->assertCount(2, $response);
 		$this->assertArrayHasKey('status', $response);
 		$this->assertEquals('error', $response['status']);
@@ -65,14 +65,14 @@ class TodosControllerTest extends WebTestCase
 
 	public function testGetTodosOffsetLt0()
 	{
-		$c = static::createClient();
-		$c->request('GET', '/todo?offset=-1');
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+		$client->request('GET', '/todo?offset=-1');
 
 		// is bad request
-		$this->assertEquals(Response::HTTP_BAD_REQUEST, $c->getResponse()->getStatusCode());
+		$this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
 
 		// test response
-		$response = json_decode($c->getResponse()->getContent(), true);
+		$response = json_decode($client->getResponse()->getContent(), true);
 		$this->assertCount(2, $response);
 		$this->assertArrayHasKey('status', $response);
 		$this->assertEquals('error', $response['status']);
@@ -80,22 +80,40 @@ class TodosControllerTest extends WebTestCase
 		$this->assertTrue(is_array($response['response']));
 	}
 
+	public function testGetTodosBlock()
+	{
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+
+		// create first 20 requests
+		for ($i = 0; $i < 20; $i++)
+		{
+			$client->request('GET', '/todo');
+
+			// all of them should be successful
+			$this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+		}
+
+		// the last one should be blocked
+		$client->request('GET', '/todo');
+		$this->assertEquals(Response::HTTP_TOO_MANY_REQUESTS, $client->getResponse()->getStatusCode());
+	}
+
 	public function testTryChangeTodo()
 	{
-		$c = static::createClient();
-		$h = [
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+		$headers = [
 			'CONTENT_TYPE' => 'application/json',
 		];
 		$body = json_encode([
 			'completed' => true,
 		]);
-		$c->request('PATCH', '/todo/1', [], [], $h, $body);
+		$client->request('PATCH', '/todo/1', [], [], $headers, $body);
 
 		// is ok
-		$this->assertEquals(Response::HTTP_OK, $c->getResponse()->getStatusCode());
+		$this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
 
 		// test response
-		$response = json_decode($c->getResponse()->getContent(), true);
+		$response = json_decode($client->getResponse()->getContent(), true);
 		$this->assertCount(2, $response);
 		$this->assertArrayHasKey('status', $response);
 		$this->assertEquals('ok', $response['status']);
@@ -106,24 +124,57 @@ class TodosControllerTest extends WebTestCase
 
 	public function tryChangeTodoUknownId()
 	{
-		$c = static::createClient();
-		$h = [
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+		$headers = [
 			'CONTENT_TYPE' => 'application/json',
 		];
 		$body = json_encode([
 			'completed' => true,
 		]);
-		$c->request('PATCH', '/todo/10000', [], [], $h, $body);
+		$client->request('PATCH', '/todo/10000', [], [], $headers, $body);
 
 		// is bad request
-		$this->assertEquals(Response::HTTP_BAD_REQUEST, $c->getResponse()->getStatusCode());
+		$this->assertEquals(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
 
 		// test response
-		$response = json_decode($c->getResponse()->getContent(), true);
+		$response = json_decode($client->getResponse()->getContent(), true);
 		$this->assertCount(2, $response);
 		$this->assertArrayHasKey('status', $response);
 		$this->assertEquals('error', $response['status']);
 		$this->assertArrayHasKey('response', $response);
 		$this->assertTrue(is_array($response['response']));
+	}
+
+	public function testTryChangeTodoBlock()
+	{
+		$client = static::createClient([], ['REMOTE_ADDR' => $this->getRandomIP()]);
+		$headers = [
+			'CONTENT_TYPE' => 'application/json',
+		];
+		$body = json_encode([
+			'completed' => true,
+		]);
+
+		// create first 20 requests
+		for ($i = 0; $i < 20; $i++)
+		{
+			$client->request('PATCH', '/todo/1', [], [], $headers, $body);
+
+			// all of them should be successful
+			$this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+		}
+
+		// the last one should be blocked
+		$client->request('PATCH', '/todo/1', [], [], $headers, $body);
+		$this->assertEquals(Response::HTTP_TOO_MANY_REQUESTS, $client->getResponse()->getStatusCode());
+	}
+
+	// -------------------------------------------------------
+	// Util Methods
+	// -------------------------------------------------------
+
+	private function getRandomIP(): string
+	{
+		return '255.' . mt_rand(0, 255) . '.' . mt_rand(0, 255) . '.' . mt_rand(0, 255);
 	}
 }
